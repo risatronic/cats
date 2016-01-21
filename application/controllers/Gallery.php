8a983e2e7d8d58ed14669fd456ed697cda5f7f03
risatronic <mcr.doig@gmail.com>
2016-01-21 14:27:39 -0800

    ACIT 4850 Lab 3 Part 2
    ^

--- application/controllers/Gallery.php
+++ application/controllers/Gallery.php
@@ -1,13 +1,14 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Gallery extends CI_Controller {
+class Gallery extends Application {
 
 	/**
 	 * Index Page for this controller.
 	 */
 	public function index()
 	{
-		$this->load->view('gallery');
+		$this->data['pagebody'] = 'gallery'; 
+                $this->render();
 	}
 }
